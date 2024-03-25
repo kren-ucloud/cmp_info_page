ce6e8191e40b57bacf1a00e0c1c4dc432c5bc3e4
Andy Yun <andy000923@gmail.com>
2024-03-25 17:54:37 +0900

    [24.03.25]info page first upload

---
+++ src/pages/InfoPage.vue
@@ -0,0 +1,195 @@
+<template>  
+    <HomeHeader/> 
+    <div class="info-container">
+        <div class="sub-visual">
+            <h2 class="sub-title">CMP 서비스</h2>
+        </div>              
+        <div class="about-container">
+            <h2>💡클라우드 관리 플랫폼(CMP)이란?</h2>
+            <p>간단한 정의로는, 기업이 클라우드 컴퓨팅 리소스를 모니터링하고 제어하는 데 사용할 수 있는 통합 소프트웨어 도구 모음이라고 할 수 있습니다.</p>
+            <h3>클라우드 관리 & 클라우드 환경을 위한 IT 관리</h3>
+            <p>클라우드 관리란, 인터넷을 통해 서버, 애플리케이션(소프트웨어), 데이터 스토리지, 네트워크 플랫폼 등의 기업 인프라 인 “클라우드”를 중앙 집중식으로 관리하는 것이다. 클라우드 환경을 위한 IT 관리의 경우, 리소스 배포, 데이터 통합, 사용 추적 및 재해 복구를 포함한 모든 유형의 비즈니스 활동을 포함할 수 있음.</p>
+            <h3>클라우드 관리 플랫폼</h3>
+            <p>엔터프라이즈 워크로드 관리 및 오케스트레이션, 위임된 관리 및 하이브리드 배포를 포함한 여러 목적을 위해 기업이 인프라, 특히 이기종 인프라를 더 효과적으로 사용할 수 있도록 도움. 그리고 사용자가 퍼블릭 클라우드의 단순성을 얻으며 IT팀이 운영 및 리스크를 통제할 수 있도록 기업의 클라우드 컴퓨팅을 지원함.</p>
+            <h3>클라우드 관리 플랫폼 도입 효과</h3>
+            <p>CMP 도입은, IT 서비스를 향상시키고 전사적으로 복잡한 비즈니스 과제를 해결하는 데 도움이 될 수 있음. 그리고, 비즈니스가 서비스 제공을 가속화하고, 다양한 IT 자산에 대한 보안을 관리 및 제어함과 동시에 전체 시스템 효율성을 향상시키고, 비즈니스 단위 간의 커뮤니케이션을 강화하도록 도움. 무엇보다, CMP를 사용하면 IT 환경에 대한 제어를 유지하고 전체 IT 비용을 절감하는 동시에 가상 솔루션 및 애플리케이션의 복잡성을 관리 할 수 있는 큰 장점이 있음.</p>
+        </div>
+        <div class="about-container2">
+            <h2>💪클라우드 관리의 5 가지 중요 요소</h2>
+            <ol>
+            <li>
+                <strong>비용 관리(Cost Management):</strong>
+                <p>
+                비용을 항목별로 관리해 상세 내역을 분석할 수 있는 툴이 필요하며, 분석 정보를 활용해 자원을 최적화할 수 있어야 함.
+                </p>
+            </li>
+            <li>
+                <strong>클라우드 보안 및 규정 준수(Cloud Security and Compliance):</strong>
+                <p>
+                클라우드 인프라, 데이터 및 기타 리소스를 보호하기 위해 중요한 부분으로, 기본적으로 계약 규정에 기반한 공동 책임 형태를 띄지만 클라우드 서비스마다 구체적인 형태는 다름.
+                </p>
+            </li>
+            <li>
+                <strong>클라우드 서비스에 대한 청구 및 비용 할당(Billing and Cost Allocation):</strong>
+                <p>
+                비용의 출처를 파악하고, 다른 부서 또는 고객에게 비용을 할당하고, 향후 비용을 예측하고 장기적으로 비용을 절감할 수 있도록 돕는 클라우드 솔루션을 확보하거나 구축하는 것은 CMP에 있어 중요한 요소임.
+                </p>
+            </li>
+            <li>
+                <strong>지원 도구(Utilization tools):</strong>
+                <p>
+                클라우드 관리에서 활동을 모니터링하고, 프로세스 흐름을 조정하고, 비용을 관리하고, 데이터를 안전하게 유지하는 등 다양한 역할을 수행하는데 활용될 수 있는 툴이 필요함.
+                </p>
+            </li>
+            <li>
+                <strong>워크플로 자동화 및 자가 치유(Workflow Automation and self-healing):</strong>
+                <p>
+                비즈니스 정책을 기반으로 클라우드 환경을 자동으로 생성, 관리 및 모니터링하는 툴이 있으므로써, 소프트웨어가 문제를 감지하고 적절한 조치를 취할 수 있어야 함.
+                </p>
+            </li>
+            </ol>
+        </div>
+    </div>
+    <div class="about-container2">
+        <img class="logo" src="../assets/info_group.png" alt="info" style="width: 900px">
+    </div>
+    <div class="signup-process-container">
+        <h2>가입 절차</h2>
+        <div class="steps">
+            <div class="circle">1<br>이메일, 기관명<br>입력</div><div class="arrow"></div>
+            <div class="circle">2<br>이메일 확인<br>및 가입</div><div class="arrow"></div>
+            <div class="circle">3<br>AWS 계정<br>연동</div><div class="arrow"></div>
+            <div class="circle">4<br>사용 준비<br>완료</div>      
+        </div>
+    </div>
+    <div class="signup-process-container">
+        <h2>해지 절차</h2>
+        <div class="steps">
+            <div class="circle">1<br>서비스 데스크에<br>해지 신청</div><div class="arrow"></div>
+            <div class="circle">2<br>최종 빌링 정보<br>확인 및 결제</div><div class="arrow"></div>
+            <div class="circle">3<br>해지 완료</div>     
+        </div>
+    </div>
+    <router-link to="/signup" class="signup-button">가입하기</router-link> 
+</template>
+  
+<script>
+import HomeHeader from "../components/Header.vue";
+export default {
+    name: 'InfoPage',
+    components: {
+        HomeHeader
+    },
+    data() {
+        return {
+        };
+    },
+    methods: {
+        showArrow(stepIndex) {
+            return stepIndex < this.steps.length;
+        }
+    }
+};
+</script>
+  
+<style scoped>
+.info-container{
+    min-width: 1000px;
+    font-family: 'NanumSquare';
+}
+.sub-visual {
+    background-image: url(../assets/sub_visual02.png);
+    background-position: center top;
+    background-size: cover;
+    width: 100%;
+    height: 440px;
+    left: 0;
+    top: 0;
+    position: absolute;
+}
+.sub-title {
+    margin-top: 200px;
+    text-align: center;
+    font-size: 3.5rem;
+    color: #fff;
+    font-weight: 700;
+}
+.about-container{
+    width: 900px;
+    margin: 500px auto 0;
+    padding: 50px;
+    align-items: center;
+    border: 1px solid #595DC4;
+    border-radius: 30px;
+    background-color: #ffffff;
+}
+.about-container2{
+    width: 900px;
+    margin: 50px auto;
+    padding: 50px;
+    align-items: center;
+    border: 1px solid #595DC4;
+    border-radius: 30px;
+    background-color: #ffffff;
+}
+.signup-process-container {
+    text-align: center;
+    width: 900px;
+    margin: 50px auto;
+    padding: 50px;
+    align-items: center;
+    border: 1px solid #595DC4;
+    border-radius: 30px;
+    background-color: #ffffff;
+    font-family: 'NanumSquare';
+}
+.steps {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 30px;
+  align-items: center;
+}
+.circle {
+  width: 140px;
+  height: 140px;
+  border: 2px solid #a3a5eb;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  font-weight: bold;
+  color: #595DC4;
+}
+.arrow{
+    content:"";
+    display: block; 
+    width: 14px; 
+    height: 14px; 
+    border: solid #595DC4; 
+    border-width: 3px 3px 0 0; 
+    transform: rotate(45deg); 
+}
+.signup-button {
+    position: fixed;
+    bottom: 30px;
+    right: 30px;
+    background: #595DC4;
+    color: #ffffff;
+    font-size: 12pt;
+    font-weight: 700;
+    font-family: "NanumSquare";
+    padding: 20px 40px;
+    border-radius: 12px;
+    display: inline-block;
+    box-shadow: 0 3px 10px rgba(9, 6, 141, 0.3);
+    z-index: 9999;
+    text-decoration: none;
+}
+.signup-button:hover {
+    background-color: #2f1976;
+}
+</style>
+
+
+  
